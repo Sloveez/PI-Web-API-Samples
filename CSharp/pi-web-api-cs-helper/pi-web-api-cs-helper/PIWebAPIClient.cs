25e55d456e9bf9da33feaeed1553fd068411172c
Daphne Ng <dng@osisoft.com>
2015-10-01 17:54:39 -0400

    Added pi-web-api-cs-helper and readwritetag projects

---
+++ CSharp/pi-web-api-cs-helper/pi-web-api-cs-helper/PIWebAPIClient.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+
+namespace pi_web_api_cs_helper
+{
+    public class PIWebAPIClient
+    {
+        private HttpClient client;
+
+        /// <summary>
+        /// Initiating HttpClient using the default credentials.
+        /// This can be used with Kerberos authentication for PI Web API.
+        /// </summary>
+        public PIWebAPIClient()
+        {
+            client = new HttpClient(new HttpClientHandler() { UseDefaultCredentials = true });
+        }
+
+        /// <summary>
+        /// Initializing HttpClient by providing a username and password. The basic authentication header is added to the HttpClient.
+        /// This can be used with Basic authentication for PI Web API.
+        /// </summary>
+        /// <param name="userName">User name for basic authentication</param>
+        /// <param name="password">Password for basic authentication</param>
+        public PIWebAPIClient(string userName, string password)
+        {
+            client = new HttpClient();
+            string authInfo = Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(String.Format("{0}:{1}", userName, password)));
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", authInfo);
+        }
+
+        /// <summary>
+        /// Async GET request. This method makes a HTTP GET request to the uri provided 
+        /// and throws an exception if the response does not indicate a success.
+        /// </summary>
+        /// <param name="uri">Endpoint for the GET request.</param>
+        /// <returns>Response from the server in a Json Object.</returns>
+        public async Task<JObject> GetAsync(string uri)
+        {
+            HttpResponseMessage response = await client.GetAsync(uri);
+
+            string content = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var responseMessage = "Response status code does not indicate success: " + (int)response.StatusCode + " (" + response.StatusCode + " ). ";
+                throw new HttpRequestException(responseMessage + Environment.NewLine + content);
+            }
+            return JObject.Parse(content);
+        }
+
+        /// <summary>
+        /// Async POST request. This method makes a HTTP POST request to the uri provided
+        /// and throws an exception if the response does not indicate a success.
+        /// </summary>
+        /// <param name="uri">Endpoint for the POST request.</param>
+        /// <param name="data">Content for the POST request.</param>
+        public async Task PostAsync(string uri, string data)
+        {
+            HttpResponseMessage response = await client.PostAsync(uri, new StringContent(data, Encoding.UTF8, "application/json"));
+
+            string content = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var responseMessage = "Response status code does not indicate success: " + (int)response.StatusCode + " (" + response.StatusCode + " ). ";
+                throw new HttpRequestException(responseMessage + Environment.NewLine + content);
+            }
+        }
+
+        /// <summary>
+        /// Calling the GetAsync method and waiting for the results.
+        /// </summary>
+        /// <param name="url">Endpoint for the GET request.</param>
+        /// <returns>Response from the server in a Json Object.</returns>
+        public JObject GetRequest(string url)
+        {
+            Task<JObject> t = this.GetAsync(url);
+            t.Wait();
+            return t.Result;
+        }
+
+        /// <summary>
+        /// Calling the PostAsync method and waiting for the results.
+        /// </summary>
+        /// <param name="url">Endpoint for the POST request.</param>
+        /// <param name="data">Content for the POST request.</param>
+        public void PostRequest(string url, string data)
+        {
+            Task t = this.PostAsync(url, data);
+            t.Wait();
+        }
+
+        /// <summary>
+        /// Disposing the HttpClient.
+        /// </summary>
+        public void Dispose()
+        {
+            client.Dispose();
+        }
+    }
+}
---
+++ CSharp/pi-web-api-cs-helper/pi-web-api-cs-readwritetag/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using pi_web_api_cs_helper;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace pi_web_api_cs_readwritetag
+{
+    class Program
+    {
+        static string baseUrl = "https://dng-code.osisoft.int/piwebapi";
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter username: ");
+            string userName = Console.ReadLine();
+            Console.Write("Enter password: ");
+            string password = GetPassword();
+            PIWebAPIClient client = new PIWebAPIClient(userName, password);
+
+            try
+            {
+                GetKST(client);
+                do
+                {
+                    string tagPath = GetTagPath();
+                    if (IsRead())
+                    {
+                        GetTagValue(client, tagPath);
+                    }
+                    else
+                    {
+                        WriteTagValue(client, tagPath);
+                    }
+                    Console.WriteLine("Press any key to continue (esc to exit)...");
+                } while (Console.ReadKey().Key != ConsoleKey.Escape);
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var e in ex.InnerExceptions)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            finally
+            {
+                client.Dispose();
+                Console.ReadKey();
+            }
+        }
+
+        // Obtain password input from console
+        private static string GetPassword()
+        {
+            string password = "";
+            ConsoleKeyInfo info = Console.ReadKey(true);
+            while (info.Key != ConsoleKey.Enter)
+            {
+                if (info.Key != ConsoleKey.Backspace)
+                {
+                    password += info.KeyChar;
+                }
+                else if (!string.IsNullOrEmpty(password))
+                {
+                    password = password.Substring(0, password.Length - 1);
+                }
+                info = Console.ReadKey(true);
+            }
+            Console.WriteLine();
+            return password;
+        }
+
+        // Display the list of PI Data Archives
+        private static void GetKST(PIWebAPIClient client)
+        {
+            Console.WriteLine("Getting list of PI Data Archives...");
+            string url = baseUrl + "/dataservers";
+            JObject jobj = client.GetRequest(url);
+            if (jobj["Items"] != null)
+            {
+                for (int i = 0, num = jobj["Items"].Count(); i < num; i++)
+                {
+                    Console.WriteLine(jobj["Items"][i]["Name"]);
+                }
+            }
+        }
+
+        // Get the name of the PI Data Archive and name of the PI tag.
+        private static string GetTagPath()
+        {
+            Console.Write("Please type in name of the PI Data Archive: ");
+            string myPI = Console.ReadLine();
+            Console.Write("Please type in name of the tag: ");
+            string myTag = Console.ReadLine();
+            return string.Format("\\\\{0}\\{1}", myPI, myTag);
+        }
+
+        // Get the most recent value of the PI tag.
+        private static void GetTagValue(PIWebAPIClient client, string path)
+        {
+            Console.WriteLine("Getting tag value...");
+            string url = GetTagUrl(client, path);
+            if (!url.Equals("Error"))
+            {
+                JObject jobj = client.GetRequest(url);
+                if (jobj["Value"] != null)
+                {
+                    Console.WriteLine("Current value = {0}", jobj["Value"]);
+                }
+            }
+        }
+
+        // Write value to the tag at current time.
+        private static void WriteTagValue(PIWebAPIClient client, string path)
+        {
+            Console.Write("Value to write? ");
+            string valueToWrite = Console.ReadLine();
+            string url = GetTagUrl(client, path);
+            if (!url.Equals("Error"))
+            {
+                object payload = new
+                {
+                    value = valueToWrite
+                };
+                string data = JsonConvert.SerializeObject(payload);
+                client.PostRequest(url, data);
+                Console.WriteLine("Value written.");
+            }
+        }
+
+        // Get the PI Web API endpoint from the tag path.
+        private static string GetTagUrl(PIWebAPIClient client, string path)
+        {
+            string url = baseUrl + "/points?path=" + path;
+            JObject jobj = client.GetRequest(url);
+            if (jobj["Links"]["Value"] != null)
+            {
+                return jobj["Links"]["Value"].ToString();
+            }
+            else
+            {
+                return "Error";
+            }
+        }
+
+        // Return true if user enters r or R, false if user enters w or W.
+        private static bool IsRead()
+        {
+            string rw;
+            bool isRead = true;
+            do
+            {
+                Console.Write("Do you wish to read or write to the tag (r/w)? ");
+                rw = Console.ReadLine();
+                if (rw.Equals("r") || rw.Equals("R"))
+                {
+                    isRead = true;
+                }
+                else if (rw.Equals("w") || rw.Equals("W"))
+                {
+                    isRead = false;
+                }
+                else
+                {
+                    Console.Write("Invalid input. ");
+                }
+            } while (!rw.Equals("r") && !rw.Equals("R") && !rw.Equals("w") && !rw.Equals("W"));
+            return isRead;
+        }
+
+    }
+}
