ca319c32e6e21006cfa44811ea6b371ff294cedb
Daphne Ng <dng@osisoft.com>
2015-10-02 15:30:42 -0400

    Added asp.net search project

--- ASPNET/pi-web-api-aspnet-search/pi-web-api-aspnet-search/Controllers/HomeController.cs
+++ ASPNET/pi-web-api-aspnet-search/pi-web-api-aspnet-search/Controllers/HomeController.cs
@@ -1,30 +1,92 @@
-﻿using System;
+﻿/***************************************************************************
+   Copyright 2015 OSIsoft, LLC.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ ***************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using pi_web_api_aspnet_search.Models;
+using pi_web_api_aspnet_search.Helpers;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace pi_web_api_aspnet_search.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            SearchModel model = new SearchModel();
+            List<SelectListItem> options = new List<SelectListItem>();
+            options.Add(new SelectListItem { Text = "all", Value = "all", Selected = true });
+            options.Add(new SelectListItem { Text = "name", Value = "name" });
+            options.Add(new SelectListItem { Text = "description", Value = "description" });
+            options.Add(new SelectListItem { Text = "afcategories", Value = "afcategories" });
+            options.Add(new SelectListItem { Text = "afelementtemplate", Value = "afelementtemplate" });
+            options.Add(new SelectListItem { Text = "attributename", Value = "attributename" });
+            options.Add(new SelectListItem { Text = "attributedescription", Value = "attributedescription" });
+            model.Options = options;
+
+            return View(model);
         }
 
-        public ActionResult About()
+        [HttpPost, ActionName("Search")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Search(SearchModel model)
         {
-            ViewBag.Message = "Your application description page.";
+            PIWebAPIClient client = new PIWebAPIClient();
+            string url = "https://myserver/piwebapi/search/query?q=";
+            if (!model.Option.Equals("all"))
+            {
+                url += model.Option + ":" + model.Query;
+            }
+            else
+            {
+                url += model.Query;
+            }
+            url += "&count=" + model.Count + "&fields=name;itemtype;webid";
 
-            return View();
+            try
+            {
+                JObject jobj = await client.GetAsync(url);
+                List<SearchResult> results = new List<SearchResult>();
+                for (int i = 0; i < jobj["Items"].Count(); i++)
+                {
+                    SearchResult result = new SearchResult();
+                    result.Name = jobj["Items"][i]["Name"].ToString();
+                    result.ItemType = jobj["Items"][i]["ItemType"].ToString();
+                    result.WebId = jobj["Items"][i]["WebID"].ToString();
+                    results.Add(result);
+                }
+                return View(results);
+            }
+            catch (Exception)
+            {
+                return View("Error");
+                throw;
+            }
+            finally
+            {
+                client.Dispose();
+            }
+            
+            
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
---
+++ ASPNET/pi-web-api-aspnet-search/pi-web-api-aspnet-search/Models/SearchModel.cs
@@ -0,0 +1,43 @@
+﻿/***************************************************************************
+   Copyright 2015 OSIsoft, LLC.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ ***************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace pi_web_api_aspnet_search.Models
+{
+    public class SearchModel
+    {
+        [Required]
+        [DisplayName("Query string")]
+        public string Query { get; set; }
+
+        [Required]
+        [DisplayName("Query field")]
+        public string Option { get; set; }
+
+        public List<SelectListItem> Options { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Please enter a positive integer.")]
+        public int? Count { get; set; }
+    }
+}
